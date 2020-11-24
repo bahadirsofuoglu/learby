48e5e83345a24402e492b62dadeddfcbefa9f324
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2020-11-25 00:37:14 +0300

    Table and edit section added to project

--- src/views/app/piaf/Start.vue
+++ src/views/app/piaf/Start.vue
@@ -70,82 +70,29 @@
 
     <b-row v-if="showMode == false">
       <b-colxx xxs="12">
-        <b-card>
-          <vuetable
-            table-height="360px"
-            ref="vuetable"
-            :api-mode="false"
-            class="order-with-arrow"
-            :fields="fields"
-            :data="cards"
-            pagination-path
-          >
-            <template slot="actions">
-              <b-row>
-                <b-colxx>
-                  <b-button size="xs" variant="outline-warning">
-                    <i class="simple-icon-pencil"
-                  /></b-button>
-                  <b-button size="xs" variant="outline-warning">
-                    <i class="simple-icon-trash"
-                  /></b-button>
-                </b-colxx>
-              </b-row>
-            </template>
-          </vuetable>
-        </b-card>
+        <Edit :cards="cards" />
       </b-colxx>
     </b-row>
   </div>
 </template>
 <script>
-import Vuetable from 'vuetable-2/src/components/Vuetable'
-
 import { mapGetters, mapActions } from 'vuex'
 import firebase from 'firebase'
+import Edit from '../table/Edit.vue'
 const db = firebase.firestore()
 
 export default {
+  props: {
+    cards: {
+      type: []
+    }
+  },
   components: {
-    vuetable: Vuetable
+    Edit
   },
   data () {
     return {
-      cards: [],
-      showMode: true,
-      fields: [
-        {
-          name: 'back',
-          sortField: 'back',
-          title: 'English',
-          titleClass: 'back',
-          dataClass: 'list-item-heading',
-          width: '30%'
-        },
-        {
-          name: 'front',
-          sortField: 'front',
-          title: 'Turkish',
-          titleClass: '',
-          dataClass: 'list-item-heading',
-          width: '30%'
-        },
-        {
-          name: 'stock',
-          sortField: 'stock',
-          title: 'Category',
-          titleClass: '',
-          dataClass: 'list-item-heading',
-          width: '20%'
-        },
-        {
-          name: '__slot:actions',
-          title: 'Actions',
-          titleClass: '',
-          dataClass: 'list-item-heading',
-          width: '20%'
-        }
-      ]
+      showMode: true
     }
   },
 
@@ -163,7 +110,6 @@ export default {
           })
         })
       })
-    console.log(this.currentUser)
   },
   computed: {
     ...mapGetters(['currentUser', 'processing', 'loginError'])
---
+++ src/views/app/table/Edit.vue
@@ -0,0 +1,76 @@
+<template>
+  <b-card>
+    <vuetable
+      table-height="360px"
+      ref="vuetable"
+      :api-mode="false"
+      class="order-with-arrow"
+      :fields="fields"
+      :data="cards"
+      pagination-path
+    >
+      <template slot="actions">
+        <b-row>
+          <b-colxx>
+            <b-button size="xs" variant="outline-warning">
+              <i class="simple-icon-pencil"
+            /></b-button>
+            <b-button size="xs" variant="outline-warning">
+              <i class="simple-icon-trash"
+            /></b-button>
+          </b-colxx>
+        </b-row>
+      </template>
+    </vuetable>
+  </b-card>
+</template>
+<script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+export default {
+  components: {
+    vuetable: Vuetable
+  },
+  props: {
+    cards: {
+      type: []
+    }
+  },
+  data () {
+    return {
+      fields: [
+        {
+          name: 'back',
+          sortField: 'back',
+          title: 'English',
+          titleClass: 'back',
+          dataClass: 'list-item-heading',
+          width: '30%'
+        },
+        {
+          name: 'front',
+          sortField: 'front',
+          title: 'Turkish',
+          titleClass: '',
+          dataClass: 'list-item-heading',
+          width: '30%'
+        },
+        {
+          name: 'stock',
+          sortField: 'stock',
+          title: 'Category',
+          titleClass: '',
+          dataClass: 'list-item-heading',
+          width: '20%'
+        },
+        {
+          name: '__slot:actions',
+          title: 'Actions',
+          titleClass: '',
+          dataClass: 'list-item-heading',
+          width: '20%'
+        }
+      ]
+    }
+  }
+}
+</script>
