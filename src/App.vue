ebcb68f21c3b9fafd68e7aa31c64f784b531f8ce
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2020-11-29 03:47:40 +0300

    some items deleted

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,25 @@
 </template>
 
 <script>
-import ColorSwitcher from "./components/Common/ColorSwitcher";
+import ColorSwitcher from './components/Common/ColorSwitcher'
 
-import { getDirection } from "./utils";
+import { getDirection } from './utils'
 
 export default {
   components: {
-    "color-switcher": ColorSwitcher
+    'color-switcher': ColorSwitcher
   },
-  beforeMount() {
-    const direction = getDirection();
+  beforeMount () {
+    const direction = getDirection()
     if (direction.isRtl) {
-      document.body.classList.add("rtl");
-      document.dir = "rtl";
-      document.body.classList.remove("ltr");
+      document.body.classList.add('rtl')
+      document.dir = 'rtl'
+      document.body.classList.remove('ltr')
     } else {
-      document.body.classList.add("ltr");
-      document.dir = "ltr";
-      document.body.classList.remove("rtl");
+      document.body.classList.add('ltr')
+      document.dir = 'ltr'
+      document.body.classList.remove('rtl')
     }
   }
-};
+}
 </script>
